ea7269ac305944805fe7af1cfaa744f5c356705f
dimitri <waelkedi@gmail.com>
2018-07-03 12:06:24 +0200

    Test git

--- src/main/java/model/Coin.java
+++ src/main/java/model/Coin.java
@@ -6,6 +6,8 @@
  * Copyright (c) 2013 Philipp Winter, Jonas Heidecke & Niklas Kaddatz         *
  ******************************************************************************/
 
+//test
+
 package model;
 
 import model.Ghost.Ghost;
